70799ada484237b1adb544db138620eab66c90a2
ConnCaster <andron5712@yandex.ru>
2025-08-16 20:52:20 +0300

    Слинкована библиотека OpenCV. Собран HW

---
+++ README.md
@@ -0,0 +1,5 @@
+## AmmoDetector
+
+### Dependencies
+- vtk  9.5.0-6
+- hdf5  1.14.6-5
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 
 int main() {
+    std::string image_path = "/home/user/dir/programming/C++/Yaroslava/AmmoDetector/images/ houses/house1.jpg";
+    Mat img = imread(image_path, IMREAD_COLOR);
 
-
-
+    imshow("Display window", img);
+    int k = waitKey(0);
     return 0;
 }
